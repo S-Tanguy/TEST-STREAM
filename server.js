a47585825b60c32287862050b324ab4a84bbf452
Tanguy STAES <tstaes-@e2r13p8.42.fr>
2018-04-26 16:14:37 +0200

    Add function search

--- server.js
+++ server.js
@@ -43,6 +43,89 @@ function searchMoviesByName(movieName, language) {
   })
 }
 
+function searchMoviesByPopularity(language, page){
+  return new Promise(function(resolve, reject) {
+    fetch('https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=c0116d807d6617f1817949aca31dd697&language=' + language + '&page=' + page)
+      .then(res => res.json())
+      .then(json => {
+        resolve(json);
+    });
+  });
+}
+
+//https://api.themoviedb.org/3/discover/movie?api_key=c0116d807d6617f1817949aca31dd697&sort_by=popularity.desc&primary_release_date.gte=2014-09-15&primary_release_date.lte=2014-10-22
+function searchMoviesBetweenDateAndListByPopularityDesc(language, page, start, end){
+  return new Promise(function(resolve, reject) {
+    fetch('https://api.themoviedb.org/3/discover/movie?api_key=c0116d807d6617f1817949aca31dd697&sort_by=popularity.desc&primary_release_date.gte=' + start +'&primary_release_date.lte=' + end + '&page=' + page + '&language=' + language)
+      .then(res => res.json())
+      .then(json => {
+        resolve(json);
+    });
+  });
+}
+
+function searchMoviesBetweenDateAndListByPopularityAsc(language, page, start, end){
+  return new Promise(function(resolve, reject) {
+    fetch('https://api.themoviedb.org/3/discover/movie?api_key=c0116d807d6617f1817949aca31dd697&sort_by=popularity.asc&primary_release_date.gte=' + start +'&primary_release_date.lte=' + end + '&page=' + page + '&language=' + language)
+      .then(res => res.json())
+      .then(json => {
+        resolve(json);
+    });
+  });
+}
+
+function searchMoviesBetweenRateAndListByPopularityDesc(language, page, start, end){
+  return new Promise(function(resolve, reject) {
+    fetch('https://api.themoviedb.org/3/discover/movie?api_key=c0116d807d6617f1817949aca31dd697&sort_by=popularity.desc&vote_average.gte=' + start +'&vote_average.lte=' + end + '&page=' + page + '&language=' + language)
+      .then(res => res.json())
+      .then(json => {
+        resolve(json);
+    });
+  });
+}
+
+function searchMoviesBetweenRateAndListByPopularityAsc(language, page, start, end){
+  return new Promise(function(resolve, reject) {
+    fetch('https://api.themoviedb.org/3/discover/movie?api_key=c0116d807d6617f1817949aca31dd697&sort_by=popularity.asc&vote_average.gte=' + start +'&vote_average.lte=' + end + '&page=' + page + '&language=' + language)
+      .then(res => res.json())
+      .then(json => {
+        resolve(json);
+    });
+  });
+}
+
+function searchMoviesInGenreAndListByPopularityDesc(language, id, page){
+  return new Promise(function(resolve, reject) {
+    fetch('https://api.themoviedb.org/3/discover/movie?api_key=c0116d807d6617f1817949aca31dd697&with_genres=' + id + '&sort_by=popularity.desc&page=' + page + '&language=' + language)
+      .then(res => res.json())
+      .then(json => {
+        resolve(json);
+    });
+  });
+}
+
+function searchMoviesInGenreAndListByPopularityAsc(language, id, page){
+  return new Promise(function(resolve, reject) {
+    fetch('https://api.themoviedb.org/3/discover/movie?api_key=c0116d807d6617f1817949aca31dd697&with_genres=' + id + '&sort_by=popularity.asc&page=' + page + '&language=' + language)
+      .then(res => res.json())
+      .then(json => {
+        resolve(json);
+    });
+  });
+}
+
+function giveGenresList(language){
+  return new Promise(function(resolve, reject) {
+    fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=c0116d807d6617f1817949aca31dd697&language=' + language)
+      .then(res => res.json())
+      .then(json => {
+        console.log(json)
+        resolve(json);
+    });
+  });
+}
+
+
 function giveDescritpionMovie(id, language) {
   return new Promise(function(resolve, reject) {
     fetch('https://api.themoviedb.org/3/movie/' + id + '?api_key=c0116d807d6617f1817949aca31dd697&language=' + language)
@@ -97,6 +180,23 @@ function subtitles_fr(url, title) {
   });
 }
 
+function convert_srtfr(title)
+{
+  var path = "./upload/subtitles/"+title+".fr.srt";
+  var dest = "./upload/subtitles/"+title+".fr.vtt";
+
+  const srtData = fs.readFileSync(path);
+  srt2vtt(srtData, (err, vttData) => {
+    if (err) connsole.log("erreur conversion fr srt to vtt" + err)
+    else {
+      fs.writeFileSync(dest, vttData);
+      // fs.unlink(path, (err) => {
+      //   if (err) console.log("erreur 2 fr srt to vtt " + err)
+      //  })
+      }
+})
+}
+
 
 function subtitles_en(url, title) {
   return new Promise((resolve, reject) => {
@@ -109,6 +209,23 @@ function subtitles_en(url, title) {
         else
           reject('dl_en_fail');
   });
+}
+
+function convert_srten(title)
+{
+  var path = "./upload/subtitles/"+title+".en.srt";
+  var dest = "./upload/subtitles/"+title+".en.vtt";
+
+  const srtData = fs.readFileSync(path);
+  srt2vtt(srtData, (err, vttData) => {
+    if (err) connsole.log("erreur conversion en srt to vtt" + err)
+    else {
+      fs.writeFileSync(dest, vttData);
+      // fs.unlink(path, (err) => {
+      //   if (err) console.log("erreur 2 en srt to vtt " + err)
+      //  })
+      }
+})
 }
 
 async function get_subtitles(Imdb, movieName)
@@ -251,11 +368,10 @@ app.get('/teststream', async function(req, res) {
 
 // C'EST LA PARTIE OU ON CONVERTIE UNE VIDEO QUI EXISTE DEJA
 // C'EST A DIRE DEUXIEME VISIONNAGE OU PLUS
-app.get('/video2', function(req, res)
+app.get('/video2', async function(req, res)
 {
-  console.log('doing');
-  transcodage('./assets/test.avi')
-  console.log('done');
+var result = await searchMoviesInGenrePopularityDesc('fr-FR', '878', 1);
+  console.log(result)
 })
 
 
